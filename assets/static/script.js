158b469c4ed93e5cbf1d21520b0ecc03c53d4e13
James Dixon <itzjamesx@gmail.com>
2019-03-21 10:32:58 -0400

    added "dark mode" with a toggle on the nav

--- assets/static/script.js
+++ assets/static/script.js
@@ -6,4 +6,37 @@ $(document).ready(function() {
             $('#back-to-top').fadeOut();
         }
     });
+    function darkoMode(){
+        let darkstylesheet = document.querySelector('link[rel="stylesheet"][href$="static/dark.css"]')
+        let darkbutton = document.getElementById("dark-button");
+        darkbutton.addEventListener("click", darktoggle);
+        if(!localStorage.getItem("dark")){
+            darkOff();
+        }else if (!isDark()){
+            darkOff();
+        }else if (isDark()){
+            darkOn();
+        }
+
+        function isDark(){
+            return localStorage.getItem("dark") == "true" ? true : false;
+        }
+        function darkOn(){
+            localStorage.setItem("dark", "true");
+            darkstylesheet.disabled = false;
+        }
+        function darkOff(){
+            localStorage.setItem("dark", "false");
+            darkstylesheet.disabled = true;
+        }
+        function darktoggle(e){
+            if (!isDark()){
+                darkOn();
+            }
+            else{
+                darkOff();
+            }
+        } 
+    }
+    darkoMode();
 });
