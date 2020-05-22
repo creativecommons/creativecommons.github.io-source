2cb6cb4035c7dd578eb3dd9fccd8849a9eacf4f6
dhruvi16 <dhruvibutti9477@gmail.com>
2020-05-22 17:23:32 +0530

    Update svg links

--- webpack/js/main.js
+++ webpack/js/main.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-    $(window).scroll(function() {
+$(document).ready(function () {
+    $(window).scroll(function () {
         if ($(this).scrollTop() > 500) {
             $('#back-to-top').fadeIn();
         } else {
@@ -7,3 +7,28 @@ $(document).ready(function() {
         }
     });
 });
+
+const getFullyQualifiedUrl = (path, version) => {
+    let baseUrl = "https://unpkg.com/@creativecommons/vocabulary"
+    if (version) {
+        baseUrl = `${baseUrl}@${version}`
+    }
+    return `${baseUrl}/${path}`
+}
+
+const patchAssetIntoDom = (asset, version = null) => {
+    const ajax = new XMLHttpRequest();
+    ajax.open("GET", getFullyQualifiedUrl(asset, version), true);
+    ajax.onload = () => {
+        var div = document.createElement("div");
+        // Render SVG in the page
+        div.innerHTML = ajax.responseText;
+        div.style.display = 'none';
+        document.body.insertBefore(div, document.body.childNodes[0]);
+    }
+
+    ajax.send();
+}
+
+patchAssetIntoDom('/assets/logos/cc/logomark.svg');
+patchAssetIntoDom('/assets/logos/cc/letterheart.svg');
