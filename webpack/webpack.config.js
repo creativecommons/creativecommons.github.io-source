83e561773a972de3499561829d009167963c9725
Dhruv Bhanushali <dhruv_b@live.com>
2020-09-06 13:08:18 +0530

    Set up the interactive page for finding new issues

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,10 +2,15 @@ const path = require('path');
 
 module.exports = {
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-  entry: ['./js/main.js','./js/ideas.js', './sass/main.scss'],
+  entry: ['./js/main.js','./js/ideas.js','./js/issue-finder.js', './sass/main.scss'],
   output: {
     path: path.resolve(__dirname, '../assets/static/gen'),
     filename: 'script.js',
+  },
+  resolve: {
+    alias: {
+      'vue$': path.resolve(__dirname, './node_modules/vue/dist/vue.esm.js')
+    }
   },
   module: {
     rules: [
